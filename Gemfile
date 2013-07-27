aeb55f5b5e6a37d364a8f42434d08d47361aff9f
Joy WANG <joy.jingyiwang@gmail.com>
2013-07-27 23:51:10 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://ruby.taobao.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.0'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.7'
+  gem 'rspec-rails'
+end
+
+group :test do
+  gem 'selenium-webdriver'
+  gem 'capybara', '2.1.0'
+end
+
+gem 'sass-rails', '4.0.0'
+gem 'uglifier'
+gem 'coffee-rails', '4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder'
+
+group :doc do
+  gem 'sdoc', '0.3.20', require: false
+end
+
+group :production do
+  gem 'pg', '0.16.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'c274b3b1bbc65f6c2b7b9778cf7a6d9a04e2c2f8d471c8cc902b388062c8d0f8dcfe1c7b6d293f525061c7ee2b049e3990118f764bd1dedb3082a9b5764c44ad'
